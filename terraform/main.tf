2a7339d4bd4ee3e5538823f2018e8e74c26f3348
hannahscovill <hnsvill@gmail.com>
2025-08-06 22:46:30 -0700

    hopefully not a circular dependency to control the cert from the tf stack

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,17 @@ resource "aws_s3_bucket" "legacy_frontend" {
   bucket = "${var.app_name}-frontend"
 }
 
-data "aws_acm_certificate" "legacy_frontend" {
-  domain = var.domain_name
+resource "aws_acm_certificate" "legacy_frontend" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.${var.domain_name}"
+  ]
+
+  tags = {
+    Name = "${var.app_name}-ssl-cert"
+  }
 }
 
 data "aws_cloudfront_cache_policy" "managed_caching_optimized" {
@@ -52,7 +61,7 @@ resource "aws_cloudfront_distribution" "legacy_frontend" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = data.aws_acm_certificate.legacy_frontend.arn
+    acm_certificate_arn      = aws_acm_certificate.legacy_frontend.arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.2_2021"
   }
